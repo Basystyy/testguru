77f6325af8ac5f6015bec601da6a3f99af6fafff
Basysty <basystyy@gmail.com>
2023-11-08 08:21:26 +0200

    Testing version

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+* Ruby version: ruby 3.1.2p20 (2022-04-12 revision 4491bb740a)
 
-* System dependencies
+* System dependencies: standart Rails and i18n
 
 * Configuration
 
--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,17 @@ module Testguru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Kyiv"
+    config.active_record.default_timezone = :utc
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
