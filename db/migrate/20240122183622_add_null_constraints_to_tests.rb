b026b05f9627f6e011c212cbd34ea55f957bf6b0
Basysty <basystyy@gmail.com>
2024-01-22 21:01:20 +0200

    Fix in tests-migrate

--- db/migrate/20240122183622_add_null_constraints_to_tests.rb
+++ db/migrate/20240122183622_add_null_constraints_to_tests.rb
@@ -2,6 +2,6 @@ class AddNullConstraintsToTests < ActiveRecord::Migration[6.1]
   def change
     change_column_null(:tests, :title, false)
     change_column_null(:tests, :level, false)
-    change_column_null(:category_id, :title, false)
+    change_column_null(:tests, category_id, false)
   end
 end
