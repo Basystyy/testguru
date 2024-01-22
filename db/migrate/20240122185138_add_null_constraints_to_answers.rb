9335dfcadeed8c3336b5176d910456160b15058b
Basysty <basystyy@gmail.com>
2024-01-22 21:09:53 +0200

    Fix in answers-migrate

--- db/migrate/20240122185138_add_null_constraints_to_answers.rb
+++ db/migrate/20240122185138_add_null_constraints_to_answers.rb
@@ -2,6 +2,6 @@ class AddNullConstraintsToAnswers < ActiveRecord::Migration[6.1]
   def change
     change_column_null(:answers, :body, false)
     change_column_null(:answers, :correct, false)
-    change_column_null(:answers, :questions_id, false)
+    change_column_null(:answers, :question_id, false)
   end
 end
