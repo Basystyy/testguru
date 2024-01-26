61ed1034437992212a054fabd6c08d66c4853b6d
Basysty <basystyy@gmail.com>
2024-01-26 11:35:36 +0200

    Creat instana and class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.name_list(name)
+    list_cat = Category.where(title: name).pluck(:id)
+    list_test = Test.select(:title).where(category_id: list_cat)
+    list_test.order(title: :desc)
+  end    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  def test_list(level)
+    Test.where(id: User.where(name: self.name).pluck(:test_id), level: level)
+  end
+  
 end
