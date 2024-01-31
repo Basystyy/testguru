9cb2c55d0660eec0274c7be16cbe20f66c1594dd
Basysty <basystyy@gmail.com>
2024-01-31 13:23:57 +0200

    Created associations category=>tests; tests=>questions; question=>answers

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
+
+  belongs_to :category
+  has_many :questions
+
   def self.name_list(name)
     list_cat = Category.where(title: name).pluck(:id)
     list_test = Test.select(:title).where(category_id: list_cat)
