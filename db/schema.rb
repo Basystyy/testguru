e9297c6a6d81dd9ecffbb3af8538a99c61f6ef71
Basysty <basystyy@gmail.com>
2024-02-03 14:00:51 +0200

    Added column email to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_31_152236) do
+ActiveRecord::Schema.define(version: 2024_02_03_115458) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2024_01_31_152236) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "email"
   end
 
   add_foreign_key "tests", "users", column: "author_id"
