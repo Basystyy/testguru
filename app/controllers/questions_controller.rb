e42330159517e1c7664293bb7ded280327816505
Basysty <basystyy@gmail.com>
2024-02-26 18:52:27 +0200

    Added form for add question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,7 @@
 class QuestionsController < ApplicationController
+
+  before_action :find_test
+
   def index
     result = Question.where(test_id: params[:test_id]).pluck(:test_id, :body).map do |list|
       list.join(' > ')
@@ -15,6 +18,20 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    # byebug
+    quest = Question.new(quest_params)
+    quest.test = @test
+    quest.save
+    render plain: quest.inspect
+  end
 
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def quest_params
+    params.require(:question).permit(:body)
   end
 end
