a884c9a5400df1e7adeb4620e7e1f1724c4fe69d
Basysty <basystyy@gmail.com>
2024-05-30 11:39:04 +0300

    Release method next_question with callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,6 +5,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_save :next_question
 
   def completed?
     current_question.nil?
@@ -15,7 +16,6 @@ class TestPassage < ApplicationRecord
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -37,7 +37,7 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
 end
