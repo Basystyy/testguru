421bcb1fe71786a930cc2314258c3b199aaad9af
Basystyy <basystyy@gmail.com>
2024-07-09 12:19:53 +0300

    Correct views flash methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
 
+  def notice_message
+    if flash[:notice]
+      content_tag(:p, flash[:notice], class: 'flash notice')
+    end
+  end
+  
+  def alert_message
+    if flash[:alert]
+      content_tag(:p, flash[:alert], class: 'flash alert')
+    end
+  end
+
   def current_year
     Time.current.year
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,3 @@
 module SessionsHelper
 
-  def alert_message(alert)
-    if flash[:alert]
-      content_tag(:p, "Are you a Guru? Verify your Email and Password please", class: 'flash alert')
-    end
-  end
-
 end
