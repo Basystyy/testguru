8fa1243d966cc21e0c4f92c4bbf735b20df76f5b
Basysty <basystyy@gmail.com>
2024-07-15 17:02:44 +0300

    Correction of comments #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def authenticate_user!
-    unless current_user
-      cookies[:last_action] = request.path
-      redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
-    end
-
-    session[:id] == current_user&.id
-  end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
