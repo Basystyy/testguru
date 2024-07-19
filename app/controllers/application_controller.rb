214706147a6444d9c4002a070703fd981801a2e2
Basysty <basystyy@gmail.com>
2024-07-19 20:51:02 +0300

    Welcome message upon login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # protect_from_forgery with: :exeption
 
   def after_sign_in_path_for(current_user)
@@ -8,6 +10,20 @@ class ApplicationController < ActionController::Base
     else
       root_path
     end
+    flash[:notice] = "Welcome, #{current_user.first_name} #{current_user.last_name}"
+    super(current_user)
+  end
+
+  def after_sign_up_path_for(current_user)
+    flash[:notice] = "Welcome, #{current_user.first_name} #{current_user.last_name}"
+    super(current_user)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 
 end
