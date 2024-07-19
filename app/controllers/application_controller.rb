f6731663181b0100cea0bef585e8042f53d54d3f
Basysty <basystyy@gmail.com>
2024-07-19 18:11:41 +0300

    Administrator`s transition to admi-resources after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
 
   # protect_from_forgery with: :exeption
 
+  def after_sign_in_path_for(current_user)
+    if current_user.is_a?(Admin)
+      admin_tests_path
+    else
+      root_path
+    end
+  end
+
 end
