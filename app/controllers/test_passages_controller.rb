48dd60b6e80c3c0e5bb9a74acef8ba8c24218100
Basysty <basystyy@gmail.com>
2024-07-23 11:33:42 +0300

    Exchange deliver_now to deliver_later

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -14,7 +14,7 @@ class TestPassagesController < AuthenticatedController
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
-      TestsMailer.completed_test(@test_passage).deliver_now
+      TestsMailer.completed_test(@test_passage).deliver_later
       redirect_to result_test_passage_path(@test_passage)
     else
       render :show
