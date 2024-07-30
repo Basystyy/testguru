3ebb5b23eed677e6d54a77d9c004c49c6417a3ba
Basysty <basystyy@gmail.com>
2024-07-30 16:59:32 +0300

    Localized view and controller of tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
   def after_sign_in_path_for(current_user)
     if current_user.is_a?(Admin)
       admin_tests_path
@@ -18,11 +20,19 @@ class ApplicationController < ActionController::Base
     super(current_user)
   end
 
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
