48ea4034e34ab62d88eb1d16446bf973da5bf9a5
Basysty <basystyy@gmail.com>
2024-07-30 13:24:37 +0300

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
 
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # protect_from_forgery with: :exeption
-
   def after_sign_in_path_for(current_user)
     if current_user.is_a?(Admin)
       admin_tests_path
