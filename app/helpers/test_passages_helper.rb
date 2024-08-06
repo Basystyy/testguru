e5c6904ca5892e91852c838094eaaabfca98f7e6
Basysty <basystyy@gmail.com>
2024-08-06 16:21:21 +0300

    Added localization for models and mailers

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,22 +1,30 @@
 module TestPassagesHelper
 
+  def header_text
+    if @test_passage.test_successfully?
+      "#{message} #{t('.right_result')}"
+    else
+      "#{message} #{t('.wrong_result')}"
+    end
+  end
+    
   def test_passage_header
     if @test_passage.test_successfully?
-      content_tag(:h2, "#{message}. Congratulations, test was completed!", class: "right")
+      content_tag(:h2, header_text, class: "right")
     else
-      content_tag(:h2, "#{message}. Better get ready, test was failed.", class: "wrong")
+      content_tag(:h2, header_text, class: "wrong")
     end
   end
 
   def message
-    "Your result: #{@test_passage.correct_questions} from #{@test_passage.test.questions.count}"
+    t('.message', your_result: @test_passage.correct_questions, max_result: @test_passage.test.questions.count)
   end
 
   def current_number
     @test_passage.test.questions.index(@test_passage.current_question) + 1
   end
 
-  def questions_coumt
+  def questions_count
     @test_passage.test.questions.count
   end
 
