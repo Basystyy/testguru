655cacd3e3b603775fbf65654611ac4f8adf58ea
Basysty <basystyy@gmail.com>
2024-11-26 12:56:59 +0200

    Backup #3

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,7 +1,22 @@
 document.addEventListener('turbolinks:load', function() {
   var control = document.querySelector('.sort-by-title')
 
-  control.addEventListener('click', function() {
-    alert('Click')
-  })
+  control.addEventListener('click', sortRowsByTitle)
 })
+
+function sortRowsByTitle() {
+  var table = document.querySelector('table')
+
+  var rows = table.querySelectorAll('tr')
+  var sortedRows = []
+
+  for (var i = 1; i < rows.length; i++) {
+    sortedRows.push(rows[i])
+  }
+
+  sortedRows.sort(compareRows)
+}
+
+function compareRows(row1, row2)
+var testTile1 = row1/querySelector('td').textContent
+var testTile2 = row2.querySelector('td').textContent
