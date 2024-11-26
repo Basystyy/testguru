0bcdb54ccc72aaac02040c5ca7205ffab9ffb1d2
Basysty <basystyy@gmail.com>
2024-11-26 12:21:49 +0200

    Backup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import '../stylesheets/application'
+import '../utilities/sorting'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
