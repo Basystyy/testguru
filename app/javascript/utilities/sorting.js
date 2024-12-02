bc314cfd0a4493a2e78d36a2e4e39923570805f1
Basysty <basystyy@gmail.com>
2024-12-02 18:35:25 +0200

    Backup ##

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,22 +1,31 @@
 document.addEventListener('turbolinks:load', function() {
   var control = document.querySelector('.sort-by-title')
 
-  control.addEventListener('click', sortRowsByTitle)
+  if (control) { control.addEventListener('click', sortRowsByTitle) }
 })
 
 function sortRowsByTitle() {
-  var table = document.querySelector('table')
+  var table = document.querySelector('tbody')
 
-  var rows = table.querySelectorAll('tr')
+  var rows = Array.from(table.querySelectorAll('tr'))
+  var header = rows.shift()
   var sortedRows = []
 
-  for (var i = 1; i < rows.length; i++) {
-    sortedRows.push(rows[i])
+  sortedRows = rows.sort(compareRows)
+
+  table.innerHTML = ''
+  table.appendChild(header)
+  for (var i = 0; i < sortedRows.length; i++) {
+    table.appendChild(sortedRows[i])
   }
 
-  sortedRows.sort(compareRows)
 }
 
-function compareRows(row1, row2)
-var testTile1 = row1/querySelector('td').textContent
-var testTile2 = row2.querySelector('td').textContent
+function compareRows(row1, row2) {
+  var testTile1 = row1.querySelector('td').textContent
+  var testTile2 = row2.querySelector('td').textContent
+
+  if (testTile1 < testTile2) { return -1 }
+  if (testTile1 > testTile2) { return 1 }
+return 0
+}
