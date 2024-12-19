b75bc0625e7bccd56e93a4b4f55d6325ace89000
Basysty <basystyy@gmail.com>
2024-12-19 14:07:01 +0200

    Sorting by title has been added to the test page for both admin and regular users

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -11,7 +11,15 @@ function sortRowsByTitle() {
   var header = rows.shift()
   var sortedRows = []
 
-  sortedRows = rows.sort(compareRows)
+  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+    sortedRows = rows.sort(compareRowsAsc)
+    this.querySelector('.octicon-arrow-up').classList.remove('hide')
+    this.querySelector('.octicon-arrow-down').classList.add('hide')
+  } else {
+    sortedRows = rows.sort(compareRowsDesc)
+    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    this.querySelector('.octicon-arrow-up').classList.add('hide')
+  } 
 
   table.innerHTML = ''
   table.appendChild(header)
@@ -21,11 +29,20 @@ function sortRowsByTitle() {
 
 }
 
-function compareRows(row1, row2) {
+function compareRowsAsc(row1, row2) {
   var testTile1 = row1.querySelector('td').textContent
   var testTile2 = row2.querySelector('td').textContent
 
   if (testTile1 < testTile2) { return -1 }
   if (testTile1 > testTile2) { return 1 }
+return 0
+}
+
+function compareRowsDesc(row1, row2) {
+  var testTile1 = row1.querySelector('td').textContent
+  var testTile2 = row2.querySelector('td').textContent
+
+  if (testTile1 < testTile2) { return 1 }
+  if (testTile1 > testTile2) { return -1 }
 return 0
 }
