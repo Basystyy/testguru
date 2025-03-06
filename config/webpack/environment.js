187f9cf1228482fefe6f6f5be5737574eb2b013c
Dmytro Angelovskyi <dmitriy.angelovskiy@gmail.com>
2025-03-06 10:39:03 +0200

    railway deploy fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,19 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['@popperjs/core', 'default']
-}))
+const webpack = require('webpack');
 
-module.exports = environment
+const sassLoader = environment.loaders.get('sass');
+sassLoader.use.find((item) => item.loader === 'sass-loader').options = {
+  implementation: require('node-sass'),
+};
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['@popperjs/core', 'default'],
+  })
+);
+
+module.exports = environment;
